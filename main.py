e5583ede10e499eb0ed60d03577406ca1fcac2d2
Hector <hectorcreatives08@gmail.com>
2024-01-25 10:51:19 -0400

    add property 9088

--- main.py
+++ main.py
@@ -702,6 +702,7 @@ def transform_xml_to_json(xml_file_path):
         {"propertyReference": "HP-9089", "propertyVisibility": "idealista", },
         {"propertyReference": "HPA-9095", "propertyVisibility": "idealista", },
         {"propertyReference": "HP-9100", "propertyVisibility": "idealista", },
+        {"propertyReference": "HP-9088", "propertyVisibility": "idealista", },
         # ... Añade más propiedades según sea necesario
     ]
 
